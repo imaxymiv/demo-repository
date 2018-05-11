151f55a47cc0796b311979d05db9fbab92b0a896
Ivan Maksymiv <ivan.maksymiv@nextiva.com>
2018-05-11 17:34:02 +0300

    second

--- src/main/java/com/example/demo/CalculatorController.java
+++ src/main/java/com/example/demo/CalculatorController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +12,7 @@ class CalculatorController {
     private Calculator calculator;
 
     @RequestMapping("/sum")
-    String sum(@RequestParam("a") Integer a,
-               @RequestParam("b") Integer b) {
+    String sum(@RequestParam("a") Integer a, @RequestParam("b") Integer b) {
         return String.valueOf(calculator.sum(a, b));
     }
 }
